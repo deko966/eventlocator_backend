38e24500e9c2d4ab37f7895b3f6c282d38a7501b
Adel Minwer <adel.minwer66@gmail.com>
2021-05-12 23:22:23 +0300

    another push

--- models/db.js
+++ models/db.js
@@ -1,13 +1,21 @@
 const mysql = require("mysql");
 const dbConfig = require("../config/DB.js");
 
+var connection;
 // Create a connection to the database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'eventlocator_dev'
+  if(process.env.JAWSDB_URL) {  
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} 
+  else {
+  //otherwise, we're going to use our local connection!  put your local db set stuff here
+  //(and remember our best practice of using the dotenv package and a .env file ;)
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'eventlocator_dev'
 });
+}
 connection.connect(error => {
     if (error) throw error;
     console.log("Successfully connected to the database.");
